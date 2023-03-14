403ccb134bacecba2e75b8660f0cff0de5e0af89
Birattam Rajak <birattam444@gmail.com>
2023-03-14 20:08:09 +0530

    comments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const btnEl = document.querySelector(".btn");
+const btnEl = document.querySelector(".btn");  //.querySelector first btn class ko return karega.
 
 btnEl.addEventListener("mouseover", (event) =>{
     // console.log(event.pageX);    console.log(event.pageY);       read more
     const x = (event.pageX -btnEl.offsetLeft);
     const y = (event.pageY -btnEl.offsetLeft);
 
-    btnEl.style.setProperty("--xPos",x+"px");
+    btnEl.style.setProperty("--xPos",x+"px");       // this setProperty method set the property of css using js
     btnEl.style.setProperty("--yPos",y+"px");
 });
