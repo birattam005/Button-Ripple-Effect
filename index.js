9f9d4824d89d951ce1b4579851b3fac1e5a1f7be
Birattam Rajak <birattam444@gmail.com>
2023-03-20 22:32:27 +0530

    fix error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const btnEl = document.querySelector(".btn");  //.querySelector first btn class 
 btnEl.addEventListener("mouseover", (event) =>{
     // console.log(event.pageX);    console.log(event.pageY);       read more
     const x = (event.pageX -btnEl.offsetLeft);
-    const y = (event.pageY -btnEl.offsetLeft);
+    const y = (event.pageY -btnEl.offsetTop);
 
     btnEl.style.setProperty("--xPos",x+"px");       // this setProperty method set the property of css using js
     btnEl.style.setProperty("--yPos",y+"px");
